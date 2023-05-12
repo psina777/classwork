47b53c17d3f3f181dc8da7a2b1881f995b573e79
lopatin <pavelop11942@mail.ru>
2023-05-12 16:25:55 +0300

    12.05.2023

---
+++ 17/17.py
@@ -0,0 +1,7 @@
+# a = [int(x) for x in open('17 (19).txt')]
+# ans = []
+# for i in range(len(a) - 1):
+#     for j in range(i + 1, len(a)):
+#         if (a[i] - a[j]) % 2 == 0 and (a[i] % 19 ==0 or a[j] % 19 == 0):
+#             ans.append((a[i] + a[j]))
+# print(len(ans), max(ans))
